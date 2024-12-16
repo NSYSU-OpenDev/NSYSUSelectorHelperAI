ebca993a41fed22c021596253b671c6b97004e38
whats2000 <whats2000mc@gmail.com>
2024-12-16 18:35:50 +0800

    Add: Add fetched data

--- backend/service/update_courses.py
+++ backend/service/update_courses.py
@@ -2,7 +2,7 @@ import json
 
 from backend.api.courses_api import NSYSUCourseAPI
 
-DATA_STORAGE_PATH = '../data/courses.json'
+DATA_STORAGE_PATH = '../data/courses.csv'
 
 def update_courses():
     """
@@ -13,9 +13,7 @@ def update_courses():
         course_data = NSYSUCourseAPI.get_latest_courses()
 
         # Store the course data in a local file
-        with open(DATA_STORAGE_PATH, 'w') as file:
-            json.dump(course_data, file, ensure_ascii=False, indent=4)
-        print(f"Course data updated and stored at {DATA_STORAGE_PATH}")
+        course_data.to_csv(DATA_STORAGE_PATH, index=False)
 
     except Exception as e:
         print(f"Failed to update course data: {str(e)}")
